4524771573dfbf4619f8650efda861516e4f4ef1
Thorinwasher <ragnarlol96@gmail.com>
2024-10-05 15:32:58 +0200

    fix compiler warning

--- src/data_types/decodec.rs
+++ src/data_types/decodec.rs
@@ -1,20 +1,28 @@
+use std::future::Future;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt, Error};
 
 pub trait Decodable: Sized {
-    async fn decode<S: AsyncReadExt + Unpin>(stream: &mut S) -> Result<Self, Error>;
+    fn decode<S: AsyncReadExt + Unpin>(stream: &mut S)
+        -> impl Future<Output = Result<Self, Error>>;
 }
 
 pub trait Encodable: Sized {
-    async fn encode<S: AsyncWriteExt + Unpin>(self: &Self, stream: &mut S) -> Result<(), Error>;
+    fn encode<S: AsyncWriteExt + Unpin>(
+        self: &Self,
+        stream: &mut S,
+    ) -> impl Future<Output = Result<(), Error>>;
 }
 
 pub trait FixedSizeDecodable<const N: usize>: Sized {
-    async fn fixed_decode<S: AsyncReadExt + Unpin>(stream: &mut S) -> Result<Self, Error>;
+    fn fixed_decode<S: AsyncReadExt + Unpin>(
+        stream: &mut S,
+    ) -> impl Future<Output = Result<Self, Error>>;
 }
 
 pub trait FixedSizeEncodable<const N: usize>: Sized {
-    async fn fixed_encode<S: AsyncWriteExt + Unpin>(
+    fn fixed_encode<S: AsyncWriteExt + Unpin>(
         self: &Self,
         stream: &mut S,
-    ) -> Result<(), Error>;
+    ) -> impl Future<Output = Result<(), Error>>;
 }
